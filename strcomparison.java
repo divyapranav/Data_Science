962023f5e51413d21e3a77232d664338d1303a67
divyapranav <dpranavashokkumar@gmail.com>
2025-02-27 11:18:56 +0530

    Add files via upload

---
+++ strcomparison.java
@@ -0,0 +1,11 @@
+public class strcomparison {
+    public static void main(String[] args) {
+        String s1 = new String("nismo");
+        String s2 = new String("gtr");
+        String s3 = new String("992 gt3rs");
+        String s4 = new String("992 gt3rs");
+        String s5 = new String("2jz");
+        System.out.println("Comparing "+s3+" and "+s4+":"+s3.equals(s4));
+    }
+    
+}
